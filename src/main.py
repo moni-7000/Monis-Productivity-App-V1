c9113ba9ff0c39591328b3609c32f020253c4044
Monica <monicaa2@umbc.edu>
2026-05-18 19:34:13 -0400

    yay button n text

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("MoniMode")
+
+label = tk.Label(root, text="Welcome to moniMode!!!! :3")
+label.pack()
+button = tk.Button(root, text="Click this")
+button.pack()
+
+root.mainloop()
