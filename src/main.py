a347dbb3baaee78925689d4f8fb45c8c98af4b00
Monica <monicaa2@umbc.edu>
2026-05-19 14:42:21 -0400

    command test

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 import tkinter as tk
+
+def buttonClicked():
+    print("You clicked the button!\nNow whats the passcode?")
+    userCode = input()
+    
+    if (userCode != "1138"):
+        print("Wrong! Sorry!")
+    
+    else:
+        print("Access Granted! Welcome to moniMode!")
+    
+
+
+
 root = tk.Tk()
 root.title("MoniMode")
+root.geometry("500x500+600+250")
 
 label = tk.Label(root, text="Welcome to moniMode!!!! :3")
 label.pack()
-button = tk.Button(root, text="Click this")
+button = tk.Button(root, command=buttonClicked, text="Click this to start!")
 button.pack()
 
 root.mainloop()
+
