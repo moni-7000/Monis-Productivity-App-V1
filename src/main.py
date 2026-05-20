b54dd82e8b215e4f05896bb8ecee57ad91b9aa77
Monica <monicaa2@umbc.edu>
2026-05-19 20:02:31 -0400

    added frames

--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
-import tkinter as tk
+import customtkinter as ctk
 
-def buttonClicked():
-    print("You clicked the button!\nNow whats the passcode?")
-    userCode = input()
-    
-    if (userCode != "1138"):
-        print("Wrong! Sorry!")
-    
-    else:
-        print("Access Granted! Welcome to moniMode!")
-    
-
-
-
-root = tk.Tk()
+root = ctk.CTk()
 root.title("MoniMode")
-root.geometry("500x500+600+250")
+root.geometry("500x500+900+300")
 
-label = tk.Label(root, text="Welcome to moniMode!!!! :3")
-label.pack()
-button = tk.Button(root, command=buttonClicked, text="Click this to start!")
-button.pack()
+start_frame = ctk.CTkFrame(root)
+main_menu_frame = ctk. CTkFrame(root)
+
+def startButtonClicked():
+    print("You clicked the button!\n")
+    showFrame(main_menu_frame)
+    start_frame.pack_forget()
+
+def showFrame(frame):
+    frame.tkraise()
+    frame.pack()
+    
+def hideFrame(frame):
+    pass
+
+showFrame(start_frame)
+startLabel = ctk.CTkLabel(start_frame, text="Welcome to moniMode!!!! :3")
+startLabel.pack()
+startButton = ctk.CTkButton(start_frame, command=startButtonClicked, text="Click this to start!")
+startButton.pack()
+
+menuLabel = ctk.CTkLabel(main_menu_frame, text="MAIN MENU\n")
+menuLabel.pack()
+calcButton = ctk.CTkButton(main_menu_frame, text="♡  CALCULATOR ♡")
+calcButton.pack()
+todoButton = ctk.CTkButton(main_menu_frame, text="♡  TO-DO LIST  ♡")
+todoButton.pack()
+focusButton = ctk.CTkButton(main_menu_frame, text="♡  FOCUS TIMER  ♡")
+focusButton.pack()
+
+
 
 root.mainloop()
 
